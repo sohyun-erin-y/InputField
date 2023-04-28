d620a22dfd57fe2f256719afb92c09b2014bea60
sohyun-erin-y <summeri9575@ewhain.net>
2023-04-29 01:41:17 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InputField from "./components/InputField";
+
+const Input = (props: any) => {
+  return (
+    <>
+      <InputField></InputField>
+    </>
+  );
+};
+
+export default Input;
---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+export default function InputField() {
+  {
+    return (
+      <form>
+        <label>
+          Enter your name:
+          <input type="text" />
+        </label>
+      </form>
+    );
+  }
+}
